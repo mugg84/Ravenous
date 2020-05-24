cdff2ba1111e1822a1da2f0f7cec8301c8e5b865
mugg84 <56695670+mugg84@users.noreply.github.com>
2020-05-24 17:56:33 +0100

    small change

--- src/util/Geolocation.js
+++
@@ -1,6 +0,0 @@
-<script>function activatePlaceSearch() {
-      let input = document.getElementById('autocomplete');
-      let autocomplete = new google.maps.places.Autocomplete(input);
-      console.log('a')
-    }</script>
- 
