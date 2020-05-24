7a966f63ae9b339efa1e4d12e33c69dd2b06fae5
mugg84 <56695670+mugg84@users.noreply.github.com>
2020-05-24 15:58:13 +0100

    solved last part, used async/await instead of then

--- src/util/Geolocation.js
+++ src/util/Geolocation.js
@@ -1,1 +1,13 @@
-
+/*
+{id: business.id,
+imageSrc: business.image_url,
+name: business.name,
+address: business.location.address1,
+city: business.location.city,
+state: business.location.dispaly_address.state,
+zipCode: business.location.dispaly_address.zip,
+category: business.categories[0].title,
+rating: business.rating,
+review: businessreview_count
+}
+*/
--- src/util/Yelp.js
+++ src/util/Yelp.js
@@ -1,2 +1,39 @@
 const apiKey =
   "B0yNoUYVXA7RVOGgVDlMj-lA5BFBc5UmpWJRieYty6DCzERgAxqfkQclNKBZcuGU1h6zZ35f1dyR4_1punYX_fALUjEZhk8nDFL_Dyc6bgVuZvRhJM7gv2LyDBC9XnYx";
+
+const Yelp = {
+  async search(term, location, sortBy) {
+    try {
+      let response = await fetch(
+        `https://cors-anywhere.herokuapp.com/https://api.yelp.com/v3/businesses/search?term=${term}&location=${location}&sort_by=${sortBy}`,
+        {
+          headers: {
+            Authorization: `Bearer ${apiKey}`,
+          },
+        }
+      );
+      let jsonResponse = await response.json();
+      if (jsonResponse.businesses) {
+        return jsonResponse.businesses.map((business) => {
+          return {
+            id: business.id,
+            imageSrc: business.image_url,
+            name: business.name,
+            address: business.location.address1,
+            city: business.location.city,
+            state: business.location.display_address.state,
+            zipCode: business.location.display_address.zip,
+            category: business.categories[0].title,
+            rating: business.rating,
+            review: business.review_count,
+          };
+        });
+      }
+      console.log("Something went wrong");
+    } catch (error) {
+      console.log(error);
+    }
+  },
+};
+
+export default Yelp;
